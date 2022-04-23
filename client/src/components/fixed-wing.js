4525f7facb60f4900fcea8a8ce5fb456cad117c7
Xenotech81 <alexradi@web.de>
2022-04-23 16:10:58 +0200

    Docstring and clean up code

--- client/src/components/fixed-wing.js
+++ client/src/components/fixed-wing.js
@@ -1,36 +1,21 @@
-// Flight dynamics of fixed-wind aircraft
+/**
+ * Flight dynamics of fixed-wind aircraft.
+ * 
+ * Computes the geometry orientation from newest and previous position vectors.
+ * Triggers on 'componentchanged' (position) event fired by aircraft entity.
+ */
 AFRAME.registerComponent('fixed-wing', {
 
-    schema: {
-        onGround: { default: false },
-    },
-
     init: function () {
-        // Closure to access fresh `this.data` from event handler context.
-        var self = this;
-        var data = this.data;
-        var el = this.el;  // Reference to the component's entity.
 
-        this.prevPosition = null;  // Vector3 to previous aircraft position
+        var self = this; // Closure to access fresh `this.data` from event handler context.
+
+        this.prevPosition = null;  // Vector3 instance of previous aircraft position
         this.orientation = new THREE.Vector3(0, 0, -1);  // Normalized orientation vector of last path segment (initiate pointing north)
-
-        // Set up initial state and variables
-        if (data.onGround) { el.addState('onGround') } else { el.addState('airborne') }
-
-        // State change listenders and handlers
-        el.addEventListener('stateadded', function (evt) {
-            if (evt.detail === 'onGround') {
-                entity.removeState('airborne');
-                console.log('Aircraft landed!');
-            } else if (evt.detail === 'airborne') {
-                entity.removeState('onGround');
-                console.log('Aircraft took off!');
-            }
-        });
 
 
         // Rotate geometry according to aircraft new and past position vectors
-        el.addEventListener('componentchanged', function (evt) {
+        this.el.addEventListener('componentchanged', function (evt) {
             if (evt.detail.name === 'position') {
                 const newPosition = evt.target.getAttribute('position')
 
@@ -41,7 +26,7 @@ AFRAME.registerComponent('fixed-wing', {
                 }
 
                 self.orientation.copy(newPosition).sub(self.prevPosition).normalize();
-                el.object3D.setRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, -1), self.orientation));
+                self.el.object3D.setRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, -1), self.orientation));
             }
         });
     },
